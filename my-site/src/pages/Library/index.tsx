9c064bb07f4f2b8a8136d4d987ae4685c783997d
wr3dny <pawel.pawtel@gmail.com>
2023-11-08 01:54:08 +0100

    7/11

--- my-site/src/pages/Library/index.tsx
+++ my-site/src/pages/Library/index.tsx
@@ -1,103 +1,44 @@
-import React, { useState, useEffect } from "react";
-import data from "./booksData.json";
-// import * as fs from "fs";
-// import "./styles.css";
+  import { useState } from "react";
+  import { Link } from "react-router-dom";
 
-console.log(data);
+  interface Book {
+    author: string;
+    title: string;
+    format: string;
+    series: string;
+    numberInSeries: string;
+  }
 
-export const Library = () => {
-  const [message, setMessage] = useState<string>("");
-  const [libraryData, setLibraryData] = useState<any[]>([]); // Define the data state
+  export const Library = () => {
 
-  // Function to read data from the JSON file
-  const loadData = async () => {
-    try {
-      const response = await fetch("booksData.json");
-      if (response.ok) {
-        const data = await response.json();
-        setLibraryData(data);
+    const [books, setBooks] = useState<Book[]>([]);
+
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        const { id, value } = e.currentTarget;
+        const newBook = { [id]: value } as Pick<Book, keyof Book>;
+        setBooks([...books, newBook]);
       }
-    } catch (error) {
-      console.error("Error reading data from the JSON file:", error);
-    }
-  };
+    };
 
-  // const fs = require("fs");
-
-  // fs.readFile("sample.json", "utf8", (err: any, data: any) => {
-  //   if (err) throw err;
-  //   const jsonObject = JSON.parse(data);
-  // });
-
-  useEffect(() => {
-    // Load data from the JSON file when the component mounts
-    loadData();
-  }, []);
-
-  // Function to write data to the JSON file
-  const saveData = () => {
-    try {
-      const jsonData = JSON.stringify(libraryData, null, 2);
-      // You can replace '/path/to/libraryData.json' with the actual file path
-      fetch("booksData.json", {
-        method: "POST",
-        body: jsonData,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    } catch (error) {
-      console.error("Error writing data to the JSON file:", error);
-    }
-  };
-
-  const [value, setValue] = useState("");
-  const onChange = (event: any) => setValue(event.target.value);
-
-  const handleCopy = (message: string) => {
-    navigator.clipboard.writeText(message);
-  };
-
-  // Function to add a new book to the library data
-  const addBook = (newBook: any) => {
-    setLibraryData((prevData) => [...prevData, newBook]);
-    saveData(); // Save the updated data to the JSON file
-  };
-
-  return (
-    <div className="library">
-      <div>Table of Owned Books</div>
-      <table className="library-table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Format</th>
-            <th>Series</th>
-            <th>Volume</th>
-            <th>Read</th>
-          </tr>
-        </thead>
-        <tbody>
-          {libraryData.map((book, index) => (
-            <tr key={index}>
-              <td>{book.title}</td>
-              <td>{book.author}</td>
-              <td>{book.format}</td>
-              <td>{book.series}</td>
-              <td>{book.volume}</td>
-              <td>{book.read ? "Yes" : "No"}</td>
-            </tr>
+    return (
+      <div className="library-main">
+        <div>Library page</div>
+        <input type="text" placeholder="author" id="author" onKeyDown={handleKeyDown} />
+        <input type="text" placeholder="title" id="title" onKeyDown={handleKeyDown} />
+        <input type="text" placeholder="format" id="format" onKeyDown={handleKeyDown} />
+        <input type="text" placeholder="series" id="series" onKeyDown={handleKeyDown} />
+        <input type="text" placeholder="numberInSerie" id="numberInSeries" onKeyDown={handleKeyDown} />
+        <ul>
+          {books.map((book, index) => (
+            <li key={index}>
+              {book.author} - {book.title} - {book.format} - {book.series} - {book.numberInSeries}
+            </li>
           ))}
-        </tbody>
-      </table>
-      <input type="text" value={value} onChange={onChange} />
+        </ul>
+        <Link to="/">Back to home</Link>
+      </div>
+    );
+  }
 
-      <button
-        onClick={() => addBook({ title: "New Book", author: "New Author" })}
-      >
-        Add New Book
-      </button>
-    </div>
-  );
-};
